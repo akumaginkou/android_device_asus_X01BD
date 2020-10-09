b1600bc8dc5e2ef6be1216af33382defd1e3c085
akumaginkou <39191737+akumaginkou@users.noreply.github.com>
2020-10-09 15:21:13 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_X01BD.mk
+    $(LOCAL_DIR)/colt_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    rr_X01BD-eng \
-    rr_X01BD-user \
-    rr_X01BD-userdebug
+    colt_X01BD-eng \
+    colt_X01BD-user \
+    colt_X01BD-userdebug
