f7aca5650190bfaea33fd31cbdfe5ecbdd0e67a4
akumaginkou <39191737+akumaginkou@users.noreply.github.com>
2020-10-09 15:34:28 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 Havoc-OS
+# Copyright (C) 2019-2020 Colt-Enigma
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
